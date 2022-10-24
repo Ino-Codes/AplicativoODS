f23c9a67a6e821986b25ada54ae19290aab26028
Ino-Codes <103116915+Ino-Codes@users.noreply.github.com>
2022-10-24 09:55:42 -0300

    final

--- AppODS/app/src/main/java/com/example/appods/MainActivity.java
+++ AppODS/app/src/main/java/com/example/appods/MainActivity.java
@@ -170,7 +170,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(tela18);
         });
 
-
         btnsair = findViewById(R.id.btnsair);
         btnsair.setOnClickListener(view -> {
             finish();
